17e2802339a065c6f29847adb23c87271bacb5aa
sairamp98 <sri.sai.ram.pothuri-1@ou.edu>
2024-08-03 17:56:19 -0500

    wrote word matching and score counting logics. Setup the basic flow of the game.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 window.addEventListener('load',init);
-let time = 0;
+let time = 6;
 let score = 0;
 let isPlaying;
 let random;
+let wordLength = 4;
 
 const wordInput = document.getElementById("word-input");
 const currentWord = document.getElementById("current-word");
@@ -10,18 +11,71 @@ const timeLeft = document.getElementById("time");
 const scoreCard = document.getElementById("score");
 const msg = document.getElementById("message");
 
-random = fetchWord(5).then(function(response) {
-    console.log(response);
-    //currentWord.innerHTML = response;
-    return response;
-});
-
 async function fetchWord(length) {
     const word = await fetch("https://random-word-api.herokuapp.com/word"+"?length="+length);
     const response = await word.json();
-    return response[0];
+    currentWord.innerHTML =  response[0];
 }
 console.log(random);
 function init(){
+    //load word on the screen
+    fetchWord(wordLength);
+    wordInput.addEventListener('input', matching);
+    setInterval(timer, 1000);
+    setInterval(checkStatus, 50);
 
 }
+function timer(){
+    if(time>0) {
+        time--;
+    }
+    else if(time===0) {
+        isPlaying =false;
+    }
+    timeLeft.innerHTML = time;
+}
+function checkStatus() {
+    if(!isPlaying && time===0) {
+        msg.innerHTML = "Game Over! Retype the word to play again."
+        wordInput.value = '';
+        score = -1;
+    }
+}
+function matching(){
+    if(wordMatch()) {
+        isPlaying = true;
+        time = 6;
+        score++;
+        if(score<=10) {
+            wordLength = 3 + Math.floor(Math.random()*3);
+            console.log(wordLength);
+        }
+        else if(score>10 && score <=20) {
+            wordLength = 3 + Math.floor(Math.random()*5);
+            console.log(wordLength);
+        }
+        else{
+            wordLength = 3 + Math.floor(Math.random()*8);
+        }
+
+        fetchWord(wordLength);
+        wordInput.value = '';
+    }
+    if(score==-1) {
+        scoreCard.innerHTML = '0';
+    }
+    else {
+        scoreCard.innerHTML = score;
+    }
+}
+function wordMatch(){
+    if(wordInput.value == currentWord.innerHTML) {
+        msg.innerHTML = 'Correct! Keep Going!';
+        return true;
+    }
+    else{
+        
+        return false;
+    }
+}
+timer();
